31ae40865e022c92dd4e27fbda44e2193a26c884
king-11 <lakshay.singh1108@gmail.com>
2020-07-30 18:30:38 +0530

    added test

--- exercises/testing/test.js
+++ exercises/testing/test.js
@@ -1,4 +1,24 @@
 // write some tests
+const {
+    findUser,
+    deleteUser
+} = require('./users')
+
+
 describe('users', () => {
-  
+    test('findUser', async () => {
+        const user = await findUser(1)
+        expect(user.id).toBe(1)
+    })
+})
+
+describe('users', () => {
+    test('findUser', async () => {
+        await deleteUser(1)
+        try {
+            const user = await findUser(1)
+        } catch (e) {
+            expect(e).toBe("No user with id 1")
+        }
+    })
 })
